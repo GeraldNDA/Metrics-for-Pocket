689eebdf3de417a8152f3fca0981d8276e27884c
GeraldNDA <gndaryee@uwaterloo.ca>
2016-09-10 22:06:07 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,183 @@
+<?php 
+  //3e5f1827-5e7a-081a-d862-cfe7d5
+  $access_token = "";
+  $consumer_key = "41234-af2d9cbd339c7ac68ff46575";
+  $pocketCount;
+  function getAuth(){
+    session_start();
+    global $consumer_key;
+    $fields_string = "";
+    if (isset($_SESSION['code'])) {
+    $code = $_SESSION['code'];
+    }
+    if(!array_key_exists("redirect",$_GET))
+    {
+    $parameters = array(
+      "consumer_key" => $consumer_key,
+      "redirect_uri" => "http://geraldnda.comlu.com/",
+    );
+
+    //set POST variables
+    $url = "https://getpocket.com/v3/oauth/request";
+
+    //url-ify the data for the POST
+    foreach($parameters as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+    $fields_string = rtrim($fields_string, '&');
+    //open connection
+    $ch = curl_init($url."?".$fields_string);
+
+    //set the url, number of POST vars, POST data
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/x-www-form-urlencoded; charset=UTF-8","X-Accept: application/json"));
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch,CURLOPT_POST, 1);
+    curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+    //execute post
+    $result = curl_exec($ch);
+    
+    if(!curl_exec($ch)){
+      //disabled error messaging
+      //die('Error: "' . curl_error($ch) . '" - Code: ' . curl_errno($ch));
+    }
+    
+    //close connection
+    curl_close($ch);
+    if(gettype($result) == "string")
+    {
+      $result = json_decode($result);
+      
+      $code = $result->code;
+      $_SESSION['code'] = $code;
+      
+      header("Location: https://getpocket.com/auth/authorize?request_token=$code&redirect_uri=http://geraldnda.comlu.com?redirect=done");
+      exit;
+    }
+    }
+    //use code to get access token
+    $url = "https://getpocket.com/v3/oauth/authorize";
+    $parameters = array(
+      "consumer_key" => $consumer_key,
+      "code" => $code,
+    );
+    foreach($parameters as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+    $fields_string = rtrim($fields_string, '&');
+    //open connection
+    $ch = curl_init($url."?".$fields_string);
+    $parameters = array(
+      "consumer_key" => $consumer_key,
+      "redirect_uri" => "http://geraldnda.comlu.com/",
+    );
+    //set the url, number of POST vars, POST data
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/x-www-form-urlencoded; charset=UTF-8","X-Accept: application/json"));
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch,CURLOPT_POST, 1);
+    curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+    //curl_setopt($ch, CURLOPT_HEADER, 1);
+    //execute post
+    $result = curl_exec($ch);
+    $result = json_decode($result);
+    if(gettype($result) == "object" )
+    {
+    global $access_token;
+    $access_token = $result->access_token;
+    global $username;
+    $username = $result->username;
+    }
+    curl_close($ch);
+  }
+    
+  function getCount() {
+    global $consumer_key;
+    global $access_token;
+    global $pocketCount;
+
+    $parameters = array(
+      "consumer_key" => $consumer_key,
+      "access_token" => $access_token,
+      "detailType" => "simple",
+    );
+    
+    //set POST variables
+    $url = 'https://getpocket.com/v3/get';
+    $fields_string = "";
+    //url-ify the data for the POST
+    foreach($parameters as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+    $fields_string = rtrim($fields_string, '&');
+    //open connection
+    $ch = curl_init($url."?".$fields_string);
+
+    //set the url, number of POST vars, POST data
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/x-www-form-urlencoded; charset=UTF-8","X-Accept: application/x-www-form-urlencoded"));
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch,CURLOPT_POST, 1);
+    curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+    //execute post
+    $result = curl_exec($ch);
+    $result =json_decode($result);
+	//echo var_dump($result);
+    $pocketCount = count((array)($result->list));
+    //$pocketCount = "6969";
+    if(!curl_exec($ch)){
+      die('Error: "' . curl_error($ch) . '" - Code: ' . curl_errno($ch));
+    }
+    //close connection
+    curl_close($ch);
+  }
+  getAuth();
+  if($access_token)
+  {
+    getCount();
+  }
+  else
+  {
+    $pocketCount = "undefined";
+  }
+?>
+
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title> Metrics for Pocket</title>
+			<script src = "http://code.jquery.com/jquery-2.1.4.min.js"></script>
+      <script> var pocketCount = <?php echo $pocketCount ?>;</script>
+      <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700|Ubuntu:400,300,300italic,500' rel='stylesheet' type='text/css'>
+      <script src = "script.js" rel = "text/javascript"></script>
+      <link href='style.css' rel='stylesheet' type='text/css'>
+		</head>
+    <body>
+      <header>
+        <div class="logo">
+          <img src="ArticleCounterLogo.svg" alt = "Logo">
+          <div class="title">
+            <h1>Metrics</h1>
+            <h2>for Pocket</h2>
+          </div>
+        </div>
+        <a href="http://geraldnda.comlu.com/" class="refresh">
+         <!--&#10227;-->
+         &#10226;
+  
+        </a>
+        <a href = "http://getpocket.com/a/queue/" class="more">
+          <div class="pocketLogo"></div>
+          <span><?php echo !empty($username)?$username:"You"; ?></span>
+        </a>
+      </header>
+        <div class="countDisplay">
+          <div class="pocketCount">
+          You have ... <span id ="swag">0</span> Unread Articles 
+          </div>
+          <div class="check">
+          Last updated <span>Unknown</span> ago ...
+          </div>
+        </div>
+      <footer>
+        made with <span class = "heart"> &#10084;</span> by Gerald Aryeetey
+      </footer>
+
+    </body>
+	</html>
+
---
+++ script.js
@@ -0,0 +1,77 @@
+var count, test;
+function increment(speed, total)
+{
+  speed = speed/0.05;
+  increment = Math.floor(total/speed);
+  test = setInterval(incrementTest, 50, increment, total);
+  document.getElementById("swag").innerHTML = 0;
+}
+function incrementTest(amount, total)
+{
+  
+  count = parseInt(document.getElementById("swag").innerHTML);
+  amount = Math.floor((Math.random() * amount))
+  if (total - count < amount)
+    {
+       amount = Math.floor((Math.random() * (total - count)))
+    }
+  if(count < total){
+  document.getElementById("swag").innerHTML = count + amount;
+  }
+  else
+    {
+      clearInterval(test);
+      if(count === 0 && location.search == "?redirect=done")
+      {
+        document.getElementById("swag").style.color = "#E94055"
+      }
+      else
+      {
+        document.getElementById("swag").style.color = "rgb(128, 255, 128)"
+      }
+    }
+}
+function addList(){
+    var list = document.getElementById("courses");
+    var course = document.getElementById("CourseAdd").value;
+    if (course && student.courses.indexOf(course) == -1) {
+        student.courses.push(course);
+        var entry = document.createElement('li');
+        var revEntry = document.createElement('li');
+
+        var del = document.createElement("button");
+        del.setAttribute("class", "remove");
+        del.onclick = function() {
+            // same content other than button tag? Deleted.
+            var course = this.parentNode.innerHTML.slice(0, -32);
+            this.parentNode.parentNode.removeChild(this.parentNode);
+            for (var i = 0; i < $("#revCourses")[0].children.length; i++) {
+                if ($("#revCourses li")[i].innerHTML == course) {
+                    $("#revCourses")[0].removeChild($("#revCourses li")[i]);
+                    student.courses.splice(i,1);
+                    break;
+                }
+            }
+        };
+
+        entry.innerHTML = course;
+        revEntry.innerHTML = course;
+
+        entry.appendChild(del);
+        list.appendChild(entry);
+        $("#revCourses")[0].appendChild(revEntry);
+    }
+
+}
+window.onload = function(){
+  
+  document.getElementById("swag").innerHTML = pocketCount;
+  increment(2, document.getElementById("swag").innerHTML);
+  //band-aid
+  window.history.replaceState("", "Metrics for Pocket", "");
+ //TO-DO:
+   //Save time of last update ...
+   //Save count as default number
+   // Add improved parsing for increased info (i.e. info about tags etc.)
+   
+}
